0eb5fddbac777855f022ff4dafebf17bf2ece444
kavantan <kavantan10@gmail.com>
2022-05-18 14:17:59 +0800

    Edited main.py and changed data.py into zoom_calls.py

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 import time
 from datetime import datetime
 from pynput.keyboard import Controller, Key
-from data import lst
+from zoom_calls import zoom_calls
 import webbrowser
+
+ZOOM_LINK = HOUR = 0
+START_TIME = MIN = 1
+END_TIME = 2
+EVENT_NAME = 3
 
 keyboard = Controller()
 
 isStarted = False
 
-for i in lst:
+for call in zoom_calls:
     while True:
         if not isStarted:
-            if datetime.now().hour == int(i[1].split(':')[0]) and datetime.now().minute == int(i[1].split(':')[1]):
-                webbrowser.open(i[0])
+            print("Waiting for " + call[EVENT_NAME] +
+                  " to start at " + call[START_TIME] + "...")
+            if datetime.now().hour == int(call[START_TIME].split(':')[HOUR]) and datetime.now().minute == int(call[START_TIME].split(':')[MIN]):
+                webbrowser.open(call[ZOOM_LINK])
                 isStarted = True
         else:
-            if datetime.now().hour == int(i[2].split(':')[0]) and datetime.now().minute == int(i[2].split(':')[1]):
-                keyboard.press('w')
+            print("Waiting for " + call[EVENT_NAME] +
+                  " to end at " + call[END_TIME] + "...")
+            if datetime.now().hour == int(call[END_TIME].split(':')[HOUR]) and datetime.now().minute == int(call[END_TIME].split(':')[MIN]):
+                keyboard.press(Key.cmd)
+                keyboard.press('w')  # Default end-call for zoom is Cmd/Ctrl W
                 time.sleep(1)
                 keyboard.press(Key.enter)
                 isStarted = False
-                break
+                break  # Move on to the next zoom call in the array
         time.sleep(5)
---
+++ zoom_calls.py
@@ -0,0 +1,4 @@
+zoom_calls = [
+    ["zoom_link1",
+        "hh:mm", "hh:mm", "title_of_event"]
+]
